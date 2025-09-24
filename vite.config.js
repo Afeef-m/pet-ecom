11a3edd46c1328bb260e56aba0014b18e523ffed
Afeef-m <afeefmaliyekkal135@gmail.com>
2025-09-24 12:37:51 +0530

    pet

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-    allowedHosts: [
-      'pet-ecom-i8wo.onrender.com',
-    ]
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3001
-  }
-})
+});
