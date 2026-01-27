8e8e371a7206e51f5e6e9a3c8629288a6f6563be
Afeef-m <afeefmaliyekkal135@gmail.com>
2026-01-27 15:55:50 +0530

    pet-ecom

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,36 @@
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config()
-const connectDB = require("./config/db")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
 const app = express();
+
+// body parsers FIRST
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// middleware
-app.use(cors())
-app.use(express.json())
+// CORS NEXT
+app.use(
+  cors({
+    origin: ["https://pet-ecom-nine.vercel.app"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+// DB
+connectDB();
 
-// connect DB
-connectDB()
+// routes
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
-app.get("/",(req, res)=>{
-    res.send("Backend is running")
-})
 app.use("/api/products", require("./routes/product.routes"));
 app.use("/api/orders", require("./routes/order.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
 
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)})
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
