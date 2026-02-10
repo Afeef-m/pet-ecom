008f179515db1f208ce266fcb6f8d669dd4f8bfe
Afeef-m <afeefmaliyekkal135@gmail.com>
2026-02-10 19:08:29 +0530

    pet-ecom

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,46 +1,3 @@
-// const express = require("express")
-// const Product = require("../models/Product")
-
-// const router = express.Router()
-
-// router.get("/",async(req,res)=>{
-//     const filter = {status:"active"}
-//     if(req.query.type)filter.type = req.query.type;
-//     if(req.query.category)filter.category = req.query.category;
-
-//     const products =await Product.find(filter)
-//     res.json(products)
-// })
-// router.get("/:id", async (req, res) => {
-//   const product = await Product.findById(req.params.id);
-
-//   if (!product) {
-//     return res.status(404).json({ message: "Product not found" });
-//   }
-
-//   res.json(product);
-// });
-
-// const auth = require("../middleware/auth.middleware");
-// const admin = require("../middleware/admin.middleware");
-
-// router.post("/", auth, admin, async (req, res) => {
-//   const product = await Product.create(req.body);
-//   res.status(201).json(product);
-// });
-
-// router.patch("/:id", auth, admin, async (req, res) => {
-//   const updatedProduct = await Product.findByIdAndUpdate(
-//     req.params.id,
-//     req.body,
-//     { new: true }
-//   );
-//   res.json(updatedProduct);
-// });
-
-// module.exports = router
-
-
 const express = require("express");
 const Product = require("../models/Product");
 
@@ -57,9 +14,6 @@ router.get("/", async (req, res) => {
     if (req.query.category) {
       filter.category = req.query.category;
     }
-
-    // ONLY add status if your schema actually supports it
-    // filter.status = "active";
 
     const products = await Product.find(filter);
     res.status(200).json(products);
