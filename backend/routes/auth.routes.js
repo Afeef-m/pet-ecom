dfb27427e691dccab0b0b4a7d0e19348631439f2
Afeef-m <afeefmaliyekkal135@gmail.com>
2026-03-03 10:40:40 +0530

    pet-ecom

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -80,16 +80,9 @@ router.post("/login", async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: "7d" }
     );
-
-   res.cookie("token", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "Strict",
-  maxAge: 7 * 24 * 60 * 60 * 1000
-});
-
 return res.json({
   message: "Login successful",
+  token,
   user: {
     _id: user._id,
     name: user.name,
